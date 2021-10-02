dec4e2957c0bbc782b268fa9236bd4f70838540c
Md Shahbaz Alam <88443812+alamshahbaz366@users.noreply.github.com>
2021-10-03 00:46:19 +0530

    Create Speed.cpp

---
+++ 01-CppBasics/Speed.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float u, v, a;  // initial_velocity, final_velocity, acceleration
+  cout << "Enter Values: ";
+  cin >> u >> v >> a;
+  
+  //Calculating speed using formula
+  int speed = ((v * v) - (u * u))/ (2 * a);
+  
+  cout << "Speed = " << speed << endl;
+  
+  return 0;
+}
+  
