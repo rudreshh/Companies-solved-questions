e1507a1b4f7f47a5fc1357a1e093b4da62569664
Md Shahbaz Alam <88443812+alamshahbaz366@users.noreply.github.com>
2021-10-06 14:35:54 +0530

    Update check palindrome.cpp

--- 01-CppBasics/check palindrome.cpp
+++ 01-CppBasics/check palindrome.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
   int x;
   cout<<"Enter any number:";
   cin>>x;
   int temp=x,r,sum=0;
+  
   while(x!=0)
   {
     r=x%10;
     sum=(sum*10)+r;
     x=x/10;
   }
+  
   if(temp==sum)
   cout<<"Yes it is a palindrome";
   else
